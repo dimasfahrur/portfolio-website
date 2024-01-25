f5f181e65c6d008433998ca5c3cd7be080ae51ff
Dimas Fr <85111246+dimasfahrur@users.noreply.github.com>
2024-01-25 19:22:49 +0700

    edit padding

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
 	return (
 		<main className='flex min-h-screen flex-col '>
 			<NavBar />
-			<div className='mt-20 container mx-auto px-4 py-4'>
+			<div className='mt-20 container mx-auto px-12 py-4'>
 				<HeroSection />
 				<AboutSection />
 				<ProjectSection />
